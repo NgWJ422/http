b4cda934f8b024cfc9fb0e636f146dc77143579f
NgWJ422 <jng010422@gmail.com>
2023-04-17 15:28:24 +0800

    mongodb update

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://jng010422:f6c2e57f6bB.@cluster0.xtnraql.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const user = await client.db("BENR2423_test").
+    collection("user").
+    insertMany(dbuser)
+    console.log(user);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 let dbuser =[
   {
       username : "soo",
